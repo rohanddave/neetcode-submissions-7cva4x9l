50b9d65c96864eb1ff0afe3615061a3f13d9fe78
Rohan Dave <rohandewangdave@gmail.com>
2026-04-06 14:20:47 -0700

    Add: search-for-word-ii - submission-3

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-3.py
@@ -0,0 +1,72 @@
+class TrieNode: 
+    def __init__(self, val):
+        self.is_end = False 
+        self.val =  val 
+        self.children = [None] * 26
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        head = TrieNode('')
+        def build_trie(head, words): 
+            for word in words: 
+                curr = head 
+                for i, char in enumerate(word): 
+                    node = None
+                    if curr.children[ord(char) - ord('a')]:
+                        node = curr.children[ord(char) - ord('a')]
+                    else:
+                        node = TrieNode(char)
+                        curr.children[ord(char) - ord('a')] = node
+                    
+                    if i == len(word) - 1:
+                        node.is_end = True
+                    curr = node
+        
+        def search(head, word): 
+            curr = head 
+            for i, char in enumerate(word): 
+                if curr.children[ord(char) - ord('a')]:
+                    curr = curr.children[ord(char) - ord('a')]
+                else:
+                    return False
+            print(f'ended at curr pointer: {curr.val}, {curr.is_end} for word {word}')
+            return curr.is_end
+                    
+        build_trie(head, words)
+        for word in words: 
+            print(f'search result for {word}: {search(head, word)}')
+        res = []
+        visited = set()
+        m, n = len(board), len(board[0])
+        # word_set = set(words) 
+
+        def dfs(r, c, curr_word, curr_trie): 
+            if not (0 <= r < m and 0 <= c < n):
+                return
+            
+            if curr_trie.is_end: 
+                res.append("".join(curr_word))
+                curr_trie.is_end = False
+                # print(f'printing visited when found word {word_string}: {visited}')
+                # return True
+
+            for dr, dc in [(1,0), (-1,0), (0,1), (0,-1)]:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < m and 0 <= nc < n and (nr,nc) not in visited and curr_trie.children[ord(board[nr][nc]) - ord('a')]:
+                    curr_word.append(board[nr][nc])
+                    visited.add((nr, nc))
+                    # prev_trie = curr_trie
+                    dfs(nr, nc, curr_word, curr_trie.children[ord(board[nr][nc]) - ord('a')])
+                    visited.remove((nr, nc))
+                    curr_word.pop()
+                    # curr_trie = prev_trie
+        for r in range(m):
+            for c in range(n): 
+                print('starting dfs at: ', (r, c))
+                curr, char = head, board[r][c]
+                if curr.children[ord(char) - ord('a')]:
+                    visited.add((r,c))
+                    dfs(r, c, [char], curr.children[ord(char) - ord('a')])
+                    visited.remove((r,c))
+        print(res)
+        return res
