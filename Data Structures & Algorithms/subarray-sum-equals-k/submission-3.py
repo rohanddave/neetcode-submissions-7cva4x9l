9c350be9ae1bdc16d05af930c715e46cca70ab77
Rohan Dave <rohandewangdave@gmail.com>
2026-04-23 14:33:27 -0700

    Add: subarray-sum-equals-k - submission-3

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-3.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        count, curr_prefix_sum, n = 0, 0, len(nums)
+        comp_map = defaultdict(int)
+        comp_map[0] = 1
+        for i in range(n):
+            curr_prefix_sum += nums[i]
+            complement = curr_prefix_sum - k 
+            if complement in comp_map: 
+                count += comp_map[complement]
+            comp_map[curr_prefix_sum] += 1
+        return count
