4ea794a169740d16c5f026e16df06891de767815
Rohan Dave <rohandewangdave@gmail.com>
2026-05-05 14:01:58 -0700

    Add: partition-equal-subset-sum - submission-2

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-2.py
@@ -0,0 +1,32 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        '''
+        we want to divide the array into two halves such that sum(half1) == sum(half2)
+        since we want only two subsets then the sum(half1) == sum(total) / 2
+        if the sum is odd then return false 
+        if the sum is even try making a sum of total / 2 
+        at each point we decide if we want to include or exclude the current number 
+        this then becomes a target sum problem where the target = total / 2
+        '''
+
+        total = sum(nums)
+
+        if total % 2 != 0: 
+            return False 
+        
+        def dfs(i, target, memo={}):
+            if target == 0: 
+                return True 
+            if i == len(nums):
+                return False 
+            if (i, target) in memo:
+                return memo[(i, target)]
+            
+            include = dfs(i + 1, target - nums[i], memo=memo)
+            exclude = dfs(i + 1, target, memo=memo)
+
+            memo[(i, target)] = include or exclude 
+            return memo[(i, target)]
+        return dfs(0, total / 2)
+
+        
